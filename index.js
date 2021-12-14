95e56a65294e9092e8a25215feb17ebc9f6cd0b5
Vimal Raj <vimalraj.nagarajan@gmail.com>
2021-12-14 19:59:09 +1300

    Factory functions

--- index.js
+++ index.js
@@ -9,3 +9,21 @@ const circle = {
 }
 
 
+//Factory function -  a way to creat an object with methods
+
+function createCircle(radius){
+  return {
+      radius: radius,
+      draw: function (){
+        console.log('draw');
+    }
+  }
+}
+
+const circle1 = createCircle(2)
+
+//construcor function - (like a class)
+
+function Circle2(radius) {
+  
+}
