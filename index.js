1292f92abcce4885314b0f680e11dd23bd328a65
Vimal Raj <vimalraj.nagarajan@gmail.com>
2021-12-14 19:58:38 +1300

    Object literal

---
+++ index.js
@@ -0,0 +1,11 @@
+//If an object has atleast one method, that object has behaviour
+//Object literals - simple way to create an object
+
+const circle = {
+  radius: 1,
+  draw: function (){
+    console.log('draw');
+  }
+}
+
+
